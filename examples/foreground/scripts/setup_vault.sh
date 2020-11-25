e0d1a46b2a4e5daf9ede9632cdc4bd6f21305ffa
Dan Farnsworth <absltkaos@gmail.com>
2020-11-25 16:46:23 -0700

    Fix typos in examples causing it to not fully work

--- examples/foreground/scripts/setup_vault.sh
+++ examples/foreground/scripts/setup_vault.sh
@@ -28,8 +28,8 @@ vault_port=$(echo $VAULT_ADDR | grep -o '[^a-zA-Z:/]\+' | tail -n 1)
 
 wait_for_listen $vault_ip $vault_port
 
-if [ -f /devlab/persistent_data/vault/env ] ; then
-    source /devlab/persistent_data/vault/env
+if [ -f /devlab/persistent_data/vault-example/env ] ; then
+    source /devlab/persistent_data/vault-example/env
     echo "Un-sealing vault..."
     unseal_out=$(vault operator unseal $UNSEAL_TOKEN 2>&1)
     rc=$?
@@ -43,16 +43,16 @@ if [ -f /devlab/persistent_data/vault/env ] ; then
     exit 1
 fi
 echo "Initializing vault..."
-vault operator init -key-shares=1 -key-threshold=1 2>&1 > /devlab/persistent_data/vault/init.out
+vault operator init -key-shares=1 -key-threshold=1 2>&1 > /devlab/persistent_data/vault-example/init.out
 
 echo "Parsing unseal and root tokens"
-export UNSEAL_TOKEN=$(cat /devlab/persistent_data/vault/init.out | grep '^Unseal Key 1: ' | awk '{print $4}')
-export VAULT_TOKEN=$(cat /devlab/persistent_data/vault/init.out | grep '^Initial Root Token: ' | awk '{print $4}')
+export UNSEAL_TOKEN=$(cat /devlab/persistent_data/vault-example/init.out | grep '^Unseal Key 1: ' | awk '{print $4}')
+export VAULT_TOKEN=$(cat /devlab/persistent_data/vault-example/init.out | grep '^Initial Root Token: ' | awk '{print $4}')
 
 echo "Writing Env file"
 VAULT_IP=$(ip addr | grep 'inet ' | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1)
 GEN_VAULT_ADDR=$(echo $VAULT_ADDR | grep -o '[htps]\+' | head -n 1)://${VAULT_IP}:$(echo $VAULT_ADDR | grep -o '[0-9]\+' | tail -n 1)
-cat <<EOF > /devlab/persistent_data/vault/env
+cat <<EOF > /devlab/persistent_data/vault-example/env
 export UNSEAL_TOKEN='${UNSEAL_TOKEN}'
 export VAULT_TOKEN='${VAULT_TOKEN}'
 export VAULT_ADDR='${GEN_VAULT_ADDR}'
@@ -78,11 +78,11 @@ EOF
 
 # Create a token for my_app to use with the new policy
 echo "Creating token for my_app to use"
-vault token create -policy=my_app -no-default-policy -period=336h 2>&1 > /devlab/persistent_data/vault/my_app_token.out
-export MY_APP_TOKEN=$(cat /devlab/persistent_data/vault/my_app_token.out | grep '^token ' | awk '{print $2}')
+vault token create -policy=my_app -no-default-policy -period=336h 2>&1 > /devlab/persistent_data/vault-example/my_app_token.out
+export MY_APP_TOKEN=$(cat /devlab/persistent_data/vault-example/my_app_token.out | grep '^token ' | awk '{print $2}')
 
 echo "Parsing and storing my_app token into env file"
-echo "export MY_APP_TOKEN='${MY_APP_TOKEN}'" >> /devlab/persistent_data/vault/env
+echo "export MY_APP_TOKEN='${MY_APP_TOKEN}'" >> /devlab/persistent_data/vault-example/env
 
 #Create a default secret kv store
 vault secrets enable -path=secret -description='Generic secrets' kv
